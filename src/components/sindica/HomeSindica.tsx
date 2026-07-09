9ac2b3fa3900517f329feeb7ad64f2bee06acfe6
JP02F <pedro73991223@gmail.com>
2026-07-09 01:15:08 -0300

    mais aperfeiçoamento

--- src/components/sindica/HomeSindica.tsx
+++ src/components/sindica/HomeSindica.tsx
@@ -12,7 +12,7 @@ export default function HomeSindica() {
       <main className="flex-1 px-6 pb-6 pt-4 z-10 flex flex-col overflow-hidden">
 
         
-        <div className="border border-zinc-300 rounded-[2.5rem] p-4 lg:p-10 w-full h-full flex flex-col bg-transparent">
+        <div className="border border-zinc-300 rounded-[1.5rem] p-4 lg:p-10 w-full h-full flex flex-col bg-transparent">
           {/* Bloco de Boas-Vindas */}
           <div className="flex items-center gap-4 mb-4 lg:mb-10">
             <Building size={56} className="text-[#741582] lg:w-14 lg:h-14 w-8 h-8" />
