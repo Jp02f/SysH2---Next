3c5ad6f11309dda55c8bf155647dcb3dde2495a2
JP02F <pedro73991223@gmail.com>
2026-07-08 23:12:22 -0300

    Refinamento na tela home do porteiro

--- src/components/porteiro/HomePorteiro.tsx
+++ src/components/porteiro/HomePorteiro.tsx
@@ -1,6 +1,7 @@
 'use client'
+import { useState } from 'react';
 import Header from '../Header';
-import { UserPlus, Users, FileText, Building } from 'lucide-react';
+import { Minus, Plus, ScanBarcode, ChevronDown } from 'lucide-react';
 import Link from 'next/link';
 import { Roboto_Flex } from 'next/font/google';
 
@@ -10,6 +11,8 @@ const robotoFlex = Roboto_Flex({
 });
 
 export default function HomePorteiro() {
+  const [quantity, setQuantity] = useState(1);
+
   return (
     <div className="h-screen bg-[#F8F9FA] flex flex-col overflow-hidden">
       <Header />
@@ -25,43 +28,148 @@ export default function HomePorteiro() {
             </div>
           </div>
 
-          {/*Formulario*/}
-          <div className="flex flex-col gap-1 flex-1 min-w-[250px]">
-            <label className="text-sm font-bold text-zinc-700 uppercase">
-                Código de Rastreio
-            </label>
-            <input
-              type="text"
-              placeholder=""
-              className="border border-zinc-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#741582] bg-white"
-            />
-          </div>
+          {/* Formulario - Linha 1 */}
+          <div className="flex flex-wrap gap-6 mt-4">
+            <div className="flex flex-col gap-1 flex-[3] min-w-0">
+              <label className="text-sm font-bold text-zinc-700 uppercase">
+                  Código de Rastreio
+              </label>
+              <div className="relative">
+                <input
+                  type="text"
+                  placeholder=""
+                  className="w-full border border-zinc-300 rounded-2xl px-4 py-2 pr-10 focus:outline-none focus:ring-2 focus:ring-[#741582] bg-white text-zinc-900"
+                />
+                <ScanBarcode size={20} className="absolute right-3 top-1/2 -translate-y-1/2 text-[#000000] pointer-events-none" />
+              </div>
+            </div>
 
-          <div className="flex flex-col gap-1 flex-1 min-w-[250px]">
-            <label className="text-sm font-bold text-zinc-700 uppercase">
-                Bloco
-            </label>
-            <select
-              className="border border-zinc-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#741582] bg-white">
-                <option value="">A</option>
-                <option value="">B</option>
-                <option value="">C</option>
-            </select>
-          </div>
+            <div className="flex flex-col gap-1 flex-[1]">
+              <label className="text-sm font-bold text-zinc-700 uppercase">
+                  Bloco
+              </label>
+              <div className="relative">
+                <select
+                  className="w-full appearance-none border border-zinc-300 rounded-2xl px-3 py-2 pr-8 focus:outline-none focus:ring-2 focus:ring-[#741582] bg-white text-zinc-900 h-[42px]">
+                  <option value="A">A</option>
+                  <option value="B">B</option>
+                  <option value="C">C</option>
+                </select>
+                <ChevronDown size={20} className="absolute right-3 top-1/2 -translate-y-1/2 text-[#000000] pointer-events-none" />
+              </div>
+            </div>
 
-          <div className="flex flex-col gap-1 flex-1 min-w-[250px]">
-            <label className="text-sm font-bold text-zinc-700 uppercase">
-                Apartamento
-            </label>
-            <select
-              className="border border-zinc-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#741582] bg-white">
-                <option value="">A</option>
-                <option value="">B</option>
-                <option value="">C</option>
-            </select>
+            <div className="flex flex-col gap-1 flex-[1]">
+              <label className="text-sm font-bold text-zinc-700 uppercase">
+                  Apartamento
+              </label>
+              <div className="relative">
+                <select
+                  className="w-full appearance-none border border-zinc-300 rounded-2xl px-3 py-2 pr-8 focus:outline-none focus:ring-2 focus:ring-[#741582] bg-white text-zinc-900 h-[42px]">
+                  <option value="11">11</option>
+                  <option value="12">12</option>
+                  <option value="13">13</option>
+                  <option value="14">14</option>
+                  <option value="21">21</option>
+                  <option value="22">22</option>
+                  <option value="23">23</option>
+                  <option value="24">24</option>
+                  <option value="31">31</option>
+                  <option value="32">32</option>
+                  <option value="33">33</option>
+                  <option value="34">34</option>  
+                  <option value="41">41</option>
+                  <option value="42">42</option>
+                  <option value="43">43</option>
+                  <option value="44">44</option>
+                  <option value="51">51</option>
+                  <option value="52">52</option>
+                  <option value="53">53</option>
+                  <option value="54">54</option>
+              </select>
+                <ChevronDown size={20} className="absolute right-3 top-1/2 -translate-y-1/2 text-[#000000] pointer-events-none" />
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-1 flex-[3]">
+              <label className="text-sm font-bold text-zinc-700 uppercase">
+                  Morador
+              </label>
+              <div className="relative">
+              <select
+                className="w-full appearance-none border border-zinc-300 rounded-2xl px-3 py-2 pr-8 focus:outline-none focus:ring-2 focus:ring-[#741582] bg-white text-zinc-900 h-[42px]">
+                  <option value="ana">Ana Giulia Belisario da Silva</option>
+                  <option value="claudio">Claudio Humberto Rodrigues Custodio</option>
+                  <option value="joao">João Pedro Ferreira de Morais</option>
+                  <option value="thalita">Thalita Domingues de Jesus</option>
+              </select>
+                <ChevronDown size={20} className="absolute right-3 top-1/2 -translate-y-1/2 text-[#000000] pointer-events-none" />
+              </div>
+            </div>
+
+            {/* Quantidade */}
+            <div className="flex flex-col gap-1 w-[150px]">
+              <label className="text-sm font-bold text-zinc-700 uppercase">
+                  Quantidade
+              </label>
+              <div className="flex items-center justify-between border border-zinc-300 rounded-2xl px-2 h-[42px] bg-white">
+                
+                <button
+                  type="button"
+                  onClick={() => setQuantity((prev) => Math.max(1, prev - 1))}
+                  className="p-2 text-zinc-600 hover:text-[#741582]"
+                  disabled={quantity <= 1}
+                >
+                  <Minus className="w-4 h-4" />
+                </button>
+
+                <span className="text-sm font-medium text-zinc-900">
+                  {quantity} Pacote(s)
+                </span>
+
+                <button
+                  type="button"
+                  onClick={() => setQuantity((prev) => Math.min(5, prev + 1))}
+                  className="p-1 text-zinc-600 hover:text-[#741582]"
+                  disabled={quantity >= 5}
+                >
+                  <Plus className="w-4 h-4" />
+                </button>
+
+              </div>
+            </div>
           </div>
+          {/* ↑ fecha a Linha 1 */}
+
+          {/* Formulario - Linha 2 */}
+          <div className="flex flex-wrap gap-6 mt-6">
+
+            <div className="flex flex-col gap-1 w-[500px]">
+              <label className="text-sm font-bold text-zinc-700 uppercase">
+                  Observação
+              </label>
+              <textarea
+                placeholder=""
+                className="border border-zinc-300 rounded-2xl px-4 py-2 h-32 resize-none focus:outline-none focus:ring-2 focus:ring-[#741582] bg-white text-zinc-900"
+              />
+            </div>
+
+            <div className="flex flex-col gap-1 w-[160px]">
+              <label className="text-sm font-bold text-zinc-700 uppercase">
+                  Data/Hora
+              </label>
+              <input
+                type="text"
+                placeholder="08/07/2026 14:30"
+                className="border border-zinc-300 rounded-2xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#741582] bg-white text-zinc-900"
+              />
+            </div>
+
+          </div>
+          {/* ↑ fecha a Linha 2 */}
 
         </div> 
+        {/* ↑ fecha o CARD */}
 
       </main> 
       
