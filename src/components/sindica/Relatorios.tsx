344a3cfee8ef6d01ec688182829bf7476a11bb09
JP02F <pedro73991223@gmail.com>
2026-07-09 13:11:50 -0300

    correção do back

---
+++ src/components/sindica/Relatorios.tsx
@@ -0,0 +1,14 @@
+'use client'
+import Header from '../Header';
+import { UserPlus, Users, FileText, Building } from 'lucide-react';
+import LogoIcon from '../logoicon';
+import Link from 'next/link';
+
+export default function Relatorios() {
+  return (
+    <div>
+      <h1>Relatórios</h1>
+      <p>Em desenvolvimento.</p>
+    </div>
+  );
+}
